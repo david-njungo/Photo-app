4b4eec99705a31fc9d5878ae026cc85e201b00ec
David Njungo <davidnjungo3@gmail.com>
2021-10-01 05:25:02 +0300

    add login authentication in views

--- igapp/views.py
+++ igapp/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from .models import Image
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def home(request):
     images = Image.get_image
     return render(request,'home.html',{"images":images}) 
+
+@login_required(login_url='/accounts/login/')
