dffe39c6c2fe437a2b7c0aaa9a2e7b64270d92b7
David Njungo <davidnjungo3@gmail.com>
2021-10-01 07:52:36 +0300

    create profile link and add background

--- igapp/urls.py
+++ igapp/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path,re_path
 
 urlpatterns=[
     url('',views.home,name ='home'),
