86690b2130a72e09dd11d4c611e948c3ece4cddd
David Njungo <davidnjungo3@gmail.com>
2021-10-01 05:28:27 +0300

    create search views

--- igapp/views.py
+++ igapp/views.py
@@ -8,3 +8,15 @@ def home(request):
     return render(request,'home.html',{"images":images}) 
 
 @login_required(login_url='/accounts/login/')
+def search_results(request):
+
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_by_name(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"projects": searched_projects})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
