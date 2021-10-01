5a63571a25817f04a0fa93cdbadc967637a231d0
David Njungo <davidnjungo3@gmail.com>
2021-10-01 05:56:30 +0300

    create test instance for comment

--- igapp/tests.py
+++ igapp/tests.py
@@ -7,3 +7,7 @@ class CommentTestClass(TestCase):
     # Set up method
     def setUp(self):
         self.comm = Comment(author = 'James', body ='Great image')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.comm,Image))
